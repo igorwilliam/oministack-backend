7fd0e7647b354a88c26f722b6b8aa90e11a43592
Igor William <igorwm9@gmail.com>
2019-04-12 14:35:25 -0300

    Fix Controller File

--- src/controllers/FileController.js
+++ src/controllers/FileController.js
@@ -3,11 +3,11 @@ const Box = require('../models/Box')
 
 class FileController {
     async store(req, res) {
-        //console.log(req.file)
+        console.log(req.file)
         const box = await Box.findById(req.params.id)
 
         const file = await File.create({
-            title: req.file.originalName,
+            title: req.file.originalname,
             path: req.file.key,
         })
 
